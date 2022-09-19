7bf77464d5826a26ece67e702596dd75ca85ee0e
Felipe González <fel.alfaro@gmail.com>
2022-09-19 02:34:22 -0300

    delete import main, change text

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from pip import main
 from pi_montecarlo import estimate_pi, estimate_pi_np
 import time
 
@@ -10,13 +9,13 @@ def main():
     pi = estimate_pi(NUM_POINTS)
     end = time.time()
 
-    print(f"python value: {pi} took: {(end-start)}")
+    print(f"python estimation: {pi} took: {(end-start)}")
 
     start = time.time()
     pi = estimate_pi_np(NUM_POINTS)
     end = time.time()
 
-    print(f"numpy value: {pi} took: {(end-start)}")
+    print(f"numpy estimation: {pi} took: {(end-start)}")
 
 if __name__ == "__main__":
     main()
